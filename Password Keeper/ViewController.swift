72760d81400f1a3b01044ffa6145e476c7944d54
blowery36 <blowery@bsd220.org>
2015-04-29 10:49:12 -0500

    Table View Basic
    got the basic table view shit

--- Password Keeper/ViewController.swift
+++ Password Keeper/ViewController.swift
@@ -8,16 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var editButton: UIBarButtonItem!
+    
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem)
+    {
+        if sender.tag == 0
+        {
+            tableView.editing = true
+            sender.tag = 1
+            
+        }
+        else
+        {
+            tableView.editing = false
+            sender.tag = 0
+        }
+        
+        
+        
+    }
+    var websites = [[String]]()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return websites.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        as UITableViewCell
+        cell.textLabel?.text = websites[indexPath.row][0]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    {
+        return  true
     }
 
-    override func didReceiveMemoryWarning() {
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
+    {
+        var website = websites[sourceIndexPath.row]
+        websites.removeAtIndex(sourceIndexPath.row)
+        websites.insert(website, atIndex: destinationIndexPath.row)
+        
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        editButton.tag = 0
+        websites.append(["Google"])
+        websites.append(["facebook"])
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
